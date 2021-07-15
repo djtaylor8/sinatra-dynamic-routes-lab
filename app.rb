d81a76f05e6358bd22ccda9bc343052e0dbf3f38
DJ Taylor <dtaylor88@gmail.com>
2021-07-15 11:42:54 -0700

    commit tree 58f3a16326ff5bf84fcffa4f4709cb61b6c33066

--- app.rb
+++ app.rb
@@ -1,6 +1,46 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do 
+    @reverse_name = params[:name].reverse
+    "#{@reverse_name}" 
+  end
+
+  get '/square/:number' do 
+    @number = params[:number].to_i ** 2
+    "#{@number}"
+  end
+
+  get '/say/:number/:phrase' do 
+    answer = ''
+    params[:number].to_i.times do 
+      answer += params[:phrase]
+    end
+    answer 
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    word_join = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]].join(" ") + "."
+    word_join
+  end
+
+  get '/:operation/:number1/:number2' do 
+    @operation = params[:operation]
+  case @operation 
+    when 'add'
+      @sum = params[:number1].to_i + params[:number2].to_i
+      "#{@sum}"
+    when 'subtract'
+      @subtract = params[:number1].to_i - params[:number2].to_i
+      "#{@subtract}"
+    when 'multiply'
+      @product = params[:number1].to_i * params[:number2].to_i
+      "#{@product}"
+    when 'divide'
+      @divide = params[:number1].to_i / params[:number2].to_i
+      "#{@divide}"
+    end
+  end
 
 end
